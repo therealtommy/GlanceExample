61f2f5c8ffc01f577c6a7b5bb41d4849cad221e3
therealtommy <sokolovai1887@ya.ru>
2025-11-30 17:42:06 +0700

    Modeling price changes

--- GlanceExample/app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
+++ GlanceExample/app/src/main/java/com/example/glanceexample/glance/StockAppWidget.kt
@@ -8,18 +8,43 @@ import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
+import androidx.glance.appwidget.updateAll
 import androidx.glance.background
 import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.time.Duration
+
 
 class StockAppWidget : GlanceAppWidget() {
-
+    private var job: Job? = null
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+
+        if (job == null) {
+            job = startUpdateJob(
+                Duration.ofSeconds(20).toMillis(),
+                context
+            )
+        }
 
         provideContent {
             GlanceTheme {
                 GlanceContent()
+            }
+        }
+    }
+    private fun startUpdateJob(timeInterval: Long, context: Context): Job {
+
+        return CoroutineScope(Dispatchers.Default).launch {
+            while (true) {
+                PriceDataRepo.update()
+                StockAppWidget().updateAll(context)
+                delay(timeInterval)
             }
         }
     }
